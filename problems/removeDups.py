e396fb2a8b957661d9aac8cd7e160a77d8b0f7f1
Jesuloba Egunjobi <jesulobaegunjobi@hotmail.com>
2019-12-14 21:36:44 +0000

    progress

--- problems/removeDups.py
+++ problems/removeDups.py
@@ -1,4 +1,46 @@
 class Node:
-  def __init__(self, data: list):
-    pass
+  def __init__(self, data = None):
+    self.data = data
+    self.next = None
+  
+  def getData(self):
+    return self.data
 
+  def getNext(self):
+    return self.next
+  
+  def setNext(self, next):
+    self.next = next
+
+  def isEmpty(self):
+    return self.getData() == None and self.getNext()
+  
+def add(head: Node, data):
+  newNode = Node(data)
+  if not head or head.isEmpty():
+    return newNode
+  
+  cursor = head
+  while cursor.getNext() != None:
+    cursor = cursor.getNext()
+  cursor.setNext(newNode)
+
+def printList(head: Node):
+  if not head:
+    raise Exception('cant print empty list')
+
+  cursor = head
+  while cursor != None:
+    print(cursor.getData())
+    cursor = cursor.getNext()
+
+def removeDup(head: Node):
+  pass
+
+if __name__ == '__main__':
+  head = Node(1)
+  # print(head.getData())
+  add(head, 2)
+  add(head, 3)
+  add(head, 4)
+  printList(head)
