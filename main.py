7170adfe4fd9915c06e8e227c3542b9d7854a98d
Jesuloba Egunjobi <jesulobaegunjobi@hotmail.com>
2019-12-23 16:52:08 +0000

    progress

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from problems.firstCommonAncestor import firstCommonAncestor
+from problems.checkSubtree import checkTree, _getTreeElements
 from ds.binaryTree import LinkedBinaryTree, Node
+from random import randint
 
 if __name__ == '__main__':
-  tree = LinkedBinaryTree()
-  for i in range(10):
-    tree.insert(Node(data=i))
+  firstTree = LinkedBinaryTree()
+  secondTree = LinkedBinaryTree()
+  for i in range(20):
+    if i < 15:
+      firstTree.insert(Node(data=randint(1, 20)))
+    else:
+      secondTree.insert(Node(data=randint(21,40)))
 
-  firstNode = Node(data=20)
-  secondNode = Node(data=-20)
+  print(_getTreeElements(firstTree.getRoot()))
+  print(_getTreeElements(secondTree.getRoot()))
 
-  tree.insert(firstNode)
-  tree.insert(secondNode)
+  firstTree.insert(secondTree.getRoot())
+  print(checkTree(firstTree.getRoot(), secondTree.getRoot()))
+  # firstTree.insert(secondTree.getRoot())
 
-  print(firstCommonAncestor(firstNode, secondNode))
+  
+
+
