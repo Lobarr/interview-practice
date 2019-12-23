7f85947794c4d22e00affe56fd291908a4be8423
Jesuloba Egunjobi <jesulobaegunjobi@hotmail.com>
2019-12-23 14:46:48 +0000

    progress

--- main.py
+++ main.py
@@ -1,35 +1,15 @@
-from queue import Queue
-from pprint import pprint
-from random import randint
-from ds.sLinkedList import SLinkedList
+from problems.firstCommonAncestor import firstCommonAncestor
 from ds.binaryTree import LinkedBinaryTree, Node
-
-def listOfDepths(tree: LinkedBinaryTree):
-  if tree.isEmpty():
-    return None
-
-  depths = {}
-  queue = Queue()
-  queue.put((tree.getRoot(), 0))
-
-  while not queue.empty():
-    cur, depth = queue.get()
-    if depth not in depths:
-      depth_list = [cur]
-      depths[depth] = depth_list
-    else:
-      depths[depth].append(cur)
-    
-    for child in cur.getChildren():
-      queue.put((child, depth + 1))
-    
-  return depths
 
 if __name__ == '__main__':
   tree = LinkedBinaryTree()
-  for i in range(20):
-    newNode = Node(data=randint(1, 100))
-    tree.insert(newNode)
+  for i in range(10):
+    tree.insert(Node(data=i))
 
-  depths = listOfDepths(tree)
-  pprint(depths)
+  firstNode = Node(data=20)
+  secondNode = Node(data=-20)
+
+  tree.insert(firstNode)
+  tree.insert(secondNode)
+
+  print(firstCommonAncestor(firstNode, secondNode))
