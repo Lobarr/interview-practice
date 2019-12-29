5df80970e211ed52fb3e8e58580be1b7f63a6a99
Jesuloba Egunjobi <jesulobaegunjobi@hotmail.com>
2019-12-29 01:44:14 +0000

    progress

---
+++ algo/bitManipulation.py
@@ -0,0 +1,32 @@
+def getBit(bits, index):
+  return (bits & (1 << index)) != 0
+
+def setBit(bits, index):
+  return bits | (1 << index)
+
+def clearBit(bits, index):
+  return bits & ~(1 << index)
+
+def clearFromLeftToIndex(bits, index):
+  mask = (1 << index) - 1
+  return bits & mask
+
+def clearBits(bits, index):
+  mask = -1 << (index + 1)
+  return bits & mask
+
+def updateBit(bits, index, newBit):
+  assert newBit == 1 or newBit == 0
+  return clearBit(bits, index) | (newBit << index)
+
+
+# Problems
+
+def insertion(N, M, i, j):
+  modifiedBits = N
+  modifierBitsIndex = 0
+  for k in range(i, j):
+    newBit = getBit(M, modifierBitsIndex)
+    modifiedBits = updateBit(N, k, newBit)
+    modifierBitsIndex += 1
+  return modifiedBits
--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-from problems.firstCommonAncestor import firstCommonAncestor
-from problems.checkSubtree import checkTree, _getTreeElements
-from ds.binaryTree import LinkedBinaryTree, Node
+from algo.bitManipulation import insertion
 from random import randint
 
 if __name__ == '__main__':
-  firstTree = LinkedBinaryTree()
-  secondTree = LinkedBinaryTree()
-  for i in range(20):
-    if i < 15:
-      firstTree.insert(Node(data=randint(1, 20)))
-    else:
-      secondTree.insert(Node(data=randint(21,40)))
-
-  print(_getTreeElements(firstTree.getRoot()))
-  print(_getTreeElements(secondTree.getRoot()))
-
-  firstTree.insert(secondTree.getRoot())
-  print(checkTree(firstTree.getRoot(), secondTree.getRoot()))
-  # firstTree.insert(secondTree.getRoot())
-
-  
-
+    N = 10000000000
+    M = 10011
+    i = 2
+    j = 6
+    
 
