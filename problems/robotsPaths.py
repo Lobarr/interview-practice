f3102354ed29e434a23e78cd164f6d4969c86ae8
Lobarr <jesulobaegunjobi@hotmail.com>
2020-04-28 16:08:34 -0700

    progress

---
+++ problems/robotsPaths.py
@@ -0,0 +1,85 @@
+# print("Hello")
+
+def robotPaths(matrix):
+    # to optimize on space, we can use given matrix to track paths instead of external bitmap structure
+
+  numRows = 0
+  numCols = 0
+  bitmap = None
+  
+  if matrix:
+    numRows = len(matrix)
+    numCols = len(matrix[0])
+    bitmap = [[False for _ in range(numCols)] for _ in range(numRows)]
+  
+  def traverse(curRowIndex, curColIndex, bitmap):
+    # base cases
+    
+    # out of bounds
+    if not (
+      0 <= curRowIndex < numRows 
+      and 0 <= curColIndex < numCols
+    ):
+      return
+      
+    # arrived at destination
+    if (
+      curRowIndex == numRows - 1 
+      and curColIndex == numCols - 1
+    ):
+      traverse.numPaths += 1
+      return
+      
+    # arrive at where we've seen
+    if bitmap[curRowIndex][curColIndex]:
+      return
+    
+    # recursive cases
+    
+    bitmap[curRowIndex][curColIndex] = True 
+    
+    traverse(curRowIndex - 1, curColIndex, bitmap) # going up
+    traverse(curRowIndex + 1, curColIndex, bitmap) # going down
+    traverse(curRowIndex, curColIndex - 1, bitmap) # going left
+    traverse(curRowIndex, curColIndex + 1, bitmap) # going right
+    
+    bitmap[curRowIndex][curColIndex] = False
+    
+  traverse.numPaths = 0
+  traverse(0, 0, bitmap)
+  
+  return traverse.numPaths
+  
+  
+example1 =  [
+  [ 0, 0, 0, 0],
+  [ 0, 0, 0, 0],
+  [ 0, 0, 0, 0]
+]
+example1_solution = 38
+
+example2 =  [
+  [ 0, 0, 0],
+  [ 0, 0, 0]
+]
+example2_solution = 4
+
+example3 =  [
+  [ 0, 0, 0, 0, 0, 0, 0, 0],
+  [ 0, 0, 0, 0, 0, 0, 0, 0],
+  [ 0, 0, 0, 0, 0, 0, 0, 0],
+  [ 0, 0, 0, 0, 0, 0, 0, 0],
+  [ 0, 0, 0, 0, 0, 0, 0, 0]
+]
+example3_solution = 7110272
+
+
+print(robotPaths(example1) == example1_solution)
+print(robotPaths(example2) == example2_solution)
+print(robotPaths(example3) == example3_solution)
+
+
+  
+  # my video has cut out; still frozen...; network on webcam computer is not connecting; will restart
+  
+  # we noticed, hop back on when you can; should we try slack?; okay lmk
---
+++ problems/search2DMatrix.py
@@ -0,0 +1,52 @@
+def search2DMatrix(matrix, target):
+
+  # uses decrease and conquer to determine potential row  
+  def binarySearchRow(_matrix, _target):
+    start = 0
+    end = len(_matrix) - 1
+    
+    while start <= end:
+      mid = start + (end - start) // 2
+      row = _matrix[mid]
+      
+      if row[0] <= _target <= row[-1]:
+        return row
+      
+      if row[0] > _target:
+        end = mid - 1
+      elif row[0] < _target:
+        start = mid + 1
+
+    return None
+
+      
+  # uses binary seaarch to find target
+  def binarySearch(arr, _target):
+    start = 0
+    end = len(arr) - 1
+    
+    while start <= end:
+      mid = start + (end - start) // 2
+      
+      if arr[mid] > _target:
+        end = mid - 1
+      elif arr[mid] < _target:
+        start = mid + 1
+      else:
+        return True
+    
+    return False
+    
+  potentialRow = binarySearchRow(matrix, target)      
+  return binarySearch(potentialRow, target) if potentialRow else False
+
+
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+
+print(search2DMatrix(matrix, 16) == True)
+print(search2DMatrix(matrix, 3) == True)
+print(search2DMatrix(matrix, 100) == False)
